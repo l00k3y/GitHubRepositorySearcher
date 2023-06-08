a2c7277f07be13a755084ce529f3ed07bd862007
Luke <lukebretherton@outlook.com>
2023-06-08 18:13:39 +0100

    Adding prettier & eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/jsx-runtime', 'airbnb'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
