9d3e5cc4abeb72ecfb89180ba01fe3bd840d68e6
Luke <lukebretherton@outlook.com>
2023-06-09 11:31:07 +0100

    Simple get repos request. Search field & button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import { TextField, Button } from '@mui/material';
+import { Octokit } from 'octokit';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [octokit, setOctoKit] = useState(null);
+
+  useEffect(() => {
+    const newOctokit = new Octokit({});
+    setOctoKit(newOctokit);
+  });
+
+  const getRepositories = () => {
+    try {
+      octokit.request('GET /repositories', {
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      }).then((response) => console.log(response));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <div>
-      <p>Hello</p>
+    <div className="fullscreen">
+      <TextField
+        label="Search Repository Name"
+        value={searchTerm}
+        onChange={(ev) => setSearchTerm(ev.target.value)}
+      />
+      <Button onClick={getRepositories}>Search</Button>
+
     </div>
   );
 }
